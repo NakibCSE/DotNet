aae6a319e17354077dea60a4928d45b5f1bbb2fc
Nakib <nakkibcse@gmail.com>
2024-12-27 15:28:06 +0600

    Linquery practice

---
+++ LinqExample/Program.cs
@@ -0,0 +1,17 @@
+﻿//Array , IEnumerable, IQueryable
+using LinqExample;
+
+List<string> names  = new List<string> { "Apple", "Mango", "Banana"};
+List<Order> orders = new List<Order>
+{
+    new Order {Item = "Banana", Quantity = 10},
+    new Order {Item = "Mango", Quantity = 20},
+    new Order {Item = "Jackfruit", Quantity =30}
+};
+
+var quantities = (from o in orders
+                  join n in names on o.Item equals n
+                  select o.Quantity);
+
+foreach (var quant in quantities)
+    Console.WriteLine(quant);
