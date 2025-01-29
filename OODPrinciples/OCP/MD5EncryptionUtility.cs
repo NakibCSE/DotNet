95b45d3dacb25aec2f1b5a955d3c5a98b23b4986
Nakib <nakkibcse@gmail.com>
2025-01-29 12:28:58 +0600

    Practice OCP design principle

---
+++ OODPrinciples/OCP/MD5EncryptionUtility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODPrinciples.OCP
+{
+    public class MD5EncryptionUtility : IEncryptionProcess
+    {
+        private readonly string _encryptionHash = "This sis is my original sis";
+        public string EncryptPassword(string password)
+        {
+            if (algorithm == "MD5")
+            {
+                //use md5 hashing algorithm
+            }
+            else if(algorithm == "SHA256")
+            { 
+                //use sha256 algorithm
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OODPrinciples/OCP/SHA256EncryptionUlility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODPrinciples.OCP
+{
+    public class SHA256EncryptionUlility : IEncryptionProcess
+    {
+        public string EncryptPassword(string password)
+        {
+            //Use sha256 hashing algorithm
+            throw new NotImplementedException();
+        }
+    }
+}
