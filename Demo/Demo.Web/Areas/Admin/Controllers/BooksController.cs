0f9bd8a153b9fd289f7a258c6af5cb8ef6414a13
Nakib <nakkibcse@gmail.com>
2025-02-27 16:07:58 +0600

    file updated

---
+++ Demo/Demo.Web/Areas/Admin/Controllers/BooksController.cs
@@ -0,0 +1,18 @@
+﻿using Demo.Web.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Web.Areas.Admin.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add(AddBookModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ Demo/Demo.Web/Areas/Admin/Models/AddBookModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo.Web.Areas.Admin.Models
+{
+    public class AddBookModel
+    {
+        public string Title { get; set; }
+        public Guid AuthorID { get; set; }
+        public DateTime PublisheDate { get; set; }
+
+    }
+}
